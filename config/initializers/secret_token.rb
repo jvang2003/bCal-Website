ea45ae9def283b4b8ee0aa43997dee5a9b5b1414
saasbook <saasbook@saasbook.(none)>
2013-10-17 17:57:15 -0700

    initial commit with User and Calendar classes made

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BCalIntegration::Application.config.secret_token = 'a172e66493427aee984d01e189e4a5be668e79488b15fd57de35da76b0a8c947423caa0e4da7657aa6a324c2522ddc5c5f97e4a70fa7b086576713588956dad7'

aa6eb16073388cbaa8d460c880ede75b87818ceb
Patrick Fong <patrickf3139@gmail.com>
2013-10-27 21:53:05 -0700

    fixed spacing in calendar_controller, did some clean up

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,37 +1,45 @@
 class CalendarController < ApplicationController
   def new
-  	render "edit" #same view as edit
+    render "edit" #same view as edit
   end
+
   def index
-	@calendars=Calendar.find(:all)
+    @calendars=Calendar.find(:all)
   end
+
   def edit
-	@calendar=Calendar.find_by_id(params[:id])
+    @calendar=Calendar.find_by_id(params[:id])
   end
+
   def create
-	Calendar.create!(:name=>params["name"],:visib => params["visib"], :key => params["key"], :fee_required => params["fee_required"], :disabled => params["disabled"])
-        flash[:notice]="Calendar has been successfully created"
-        redirect_to '/'
+    Calendar.create!(:name=>params["name"],:visib => params["visib"], :key => params["key"], :fee_required => params["fee_required"], :disabled => params["disabled"])
+    flash[:notice]="Calendar has been successfully created"
+    redirect_to '/'
   end
+
   def show
-	@calendar=Calendar.find(params[:id])
+    @calendar=Calendar.find(params[:id])
   end
+
   def destroy
-	@calendar=Calendar.find(params[:id])
-        @calendar.destroy
-        flash[:notice]="Calendar has been deleted"
-        redirect_to '/'
+    @calendar=Calendar.find(params[:id])
+    @calendar.destroy
+    flash[:notice]="Calendar has been deleted"
+    redirect_to '/'
   end
+
   def update
-	id=params[:id]
-	@calendar=Calendar.find_by_id(id)
-        @calendar.name=params["name"]
-        @calendar.visib=params["visib"]
-        @calendar.key=params["key"]
-        @calendar.fee_required=params["fee_required"]
-        @calendar.disabled=params["disabled"]
-	@calendar.save!
-        flash[:notice]="Calendar has been successfully updated"
-        redirect_to '/'
+    id=params[:id]
+    @calendar=Calendar.find_by_id(id)
+
+    @calendar.name=params["name"]
+    @calendar.visib=params["visib"]
+    @calendar.key=params["key"]
+    @calendar.fee_required=params["fee_required"]
+    @calendar.disabled=params["disabled"]
+    @calendar.save!
+
+    flash[:notice]="Calendar has been successfully updated"
+    redirect_to '/'
   end
 end

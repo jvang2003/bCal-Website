be0f5ab40304308a12857d2cb5cab3e5b76d90e9
saasbook <saasbook@saasbook.(none)>
2013-11-04 00:32:25 -0800

    feature and step definition for filtering

---
+++ features/step_definitions/public_user_view.rb
@@ -0,0 +1,16 @@
+When /^I try to search by (.*) with "(.*)"/ do |attr, value|
+	# type in value in attr search box
+	fill_in attr, :with => value
+	# click filter button to filter
+	click_button 'Filter'
+end
+
+Then /I should see calendar "([^\"]*)"/ do |calendar_name|
+  if page.respond_to? :should
+    page.should have_content(calendar_name)
+  else
+    assert page.has_content?(calendar_name)
+  end
+end
+
+

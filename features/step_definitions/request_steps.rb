68a0393c34d1be63a442ed592d0783ca7b54f981
Bryan Chu <bryanchu08@yahoo.com>
2013-11-03 17:39:38 -0800

    add tests for request dumping

---
+++ features/step_definitions/request_steps.rb
@@ -0,0 +1,23 @@
+Given /the following requests exist/ do |requsts_table| #done
+    requests_table.hashes.each do |request|
+        Request.create!(request)
+    end
+end
+
+When /I view my request history/ do 
+	click_button("View requests")
+end
+
+When /I filter by (.*)/ do |filter|
+	find("option[value='#{filter}']").click
+end
+
+Then /I should see room "([^\"]*)" with status "([^\"]*)"/ do |name, status|
+	el = page.all("tr", :text => name)
+	el.should_not be_nil
+	el.first.native.text.should include status
+end
+
+Then /I should not see room "([^\"]*)"/ do |name|
+	page.all("tr", :text => name).should be_nil
+end

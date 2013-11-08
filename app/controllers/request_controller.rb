525e23b3166cc0f81dd10dc983e19d396a8ee13b
Stephen Martinis <moowiz2020@gmail.com>
2013-11-07 19:48:14 -0800

    Added migrate, modified the request controller

--- app/controllers/request_controller.rb
+++ app/controllers/request_controller.rb
@@ -1,11 +1,22 @@
 class RequestController < ApplicationController
+  before_action :check_auth
+
   def new
   end
+
   def create
-    time=DateTime.new(params["date"]["(1i)"].to_i,params["date"]["(2i)"].to_i,params["date"]["(3i)"].to_i,params["time"]["(4i)"].to_i,params["time"]["(5i)"].to_i)
-    Request.create!(:details=>params["details"],:people => params["people"], :reason => params["reason"],:place => params["place"],:time=>time)
+    to_pass = params.extract! %w[people details reason place]
+    to_pass[:time] = DateTime.new(params["date"]["(1i)"].to_i,params["date"]["(2i)"].to_i,params["date"]["(3i)"].to_i,params["time"]["(4i)"].to_i,params["time"]["(5i)"].to_i)
+    Request.create! to_pass
     RequestMailer.request_successful(params).deliver
     flash[:notice]="Request has been submitted"
     redirect_to calendars_path
+  end
+
+  def check_auth
+  	res = RequestHelper::check_auth
+  	if res != nil
+  		redirect_to res
+  	end
   end
 end

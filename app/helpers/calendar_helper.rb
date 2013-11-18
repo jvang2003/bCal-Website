cda75e20f4b3d7b653498964c57ef6f5eaed565a
Patrick Fong <patrickf3139@gmail.com>
2013-11-17 23:19:51 -0800

    removed some unused code in calendar_helper to increase coverage

--- app/helpers/calendar_helper.rb
+++ app/helpers/calendar_helper.rb
@@ -1,14 +1,4 @@
 require 'google/api_client'
 
 module CalendarHelper
-	def CalendarHelper::calendar
-		@calendar
-	end
-
-	def CalendarHelper::check_auth redirect_uri
-		if @client == nil
-			return CalendarHelper::init redirect_uri
-		end
-		return nil
-	end
 end

3f88157d63ab31f48ecc542fb74e898a4a275819
Stephen Martinis <moowiz2020@gmail.com>
2013-11-24 22:38:59 -0800

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131118013907) do
+ActiveRecord::Schema.define(:version => 20131125060506) do
 
   create_table "calendars", :force => true do |t|
     t.string   "name"
@@ -52,11 +52,15 @@ ActiveRecord::Schema.define(:version => 20131118013907) do
   end
 
   create_table "users", :force => true do |t|
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
     t.string   "name"
-    t.string   "password"
-    t.string   "email"
+    t.string   "remember_token"
+    t.string   "calnet_id"
+    t.integer  "role"
   end
+
+  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
+  add_index "users", ["role"], :name => "index_users_on_role"
 
 end

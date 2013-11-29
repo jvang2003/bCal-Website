1fd740708739ca8b6866e437964bf5bb7e653e47
Patrick Fong <patrickf3139@gmail.com>
2013-11-29 15:09:46 -0800

    forgot to include steps haha...

---
+++ features/step_definitions/export_steps.rb
@@ -0,0 +1,21 @@
+# search for value in a CSV file
+Then /I should see in the (?i:CSV) the event "(.*)"/ do |event|
+	# parse view into rows and then values
+	rows = page.body.split("\n")
+	rows.length.should > 1
+	csv = rows.map {|row| row.split(",")}
+
+	# find the event in the name column
+	name_index = csv[0].index("Name")
+	name_col = []
+	csv[1..-1].each {|row| name_col << row[name_index]}
+	name_col.should.include event
+end
+
+And /I export (?:it|page|calendar) to (?i:CSV)/ do
+	click_on("Export to CSV")
+end
+
+And /I export (?:it|page|calendar) to (?i:PDF)/ do
+	click_on("Export to PDF")
+end

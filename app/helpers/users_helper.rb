bebee44b47b95ff0839217cae1e6a6ad64252411
Stephen Martinis <moowiz2020@gmail.com>
2013-12-04 11:36:03 -0800

    A little refactoring

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,21 +6,18 @@ end
 
 module UsersHelper
   def valid_create_roles_for user
-    if user == nil
-      return User.VALID_ROLES.to_a[0, 1]
-    end
+    return User.VALID_ROLES.to_a[0, 1] unless user
 
     index = 0
     arr = User.VALID_ROLES.to_a
     while arr[index][1] <= user.role
       index += 1
     end
-    return arr[0,index]
+    arr[0,index]
   end
 
   def check_signed_in_user?
     unless signed_in? or is_same_controller_and_action?(request.original_url, sign_up_path)
-      puts "uh oh"
       redirect_to sign_in_url(:params => {:return_to => request.original_url}), notice: "Please sign in."
     else
       nil
@@ -31,7 +28,7 @@ module UsersHelper
     signed_in? and current_user.role > User.VALID_ROLES["Guest"]
   end
 
-  ["App Admin", "Dept Admin"].each do |name|
+  ["App Admin", "Dept Admin", "Admin"].each do |name|
     define_method "is_#{name.to_slug}?" do
       return if check_signed_in_user?
       if current_user.role < User.VALID_ROLES[name]
@@ -42,10 +39,8 @@ module UsersHelper
   end
 
   def is_higher_admin?
-    logger.info "hihi"
     @user ||= User.find params[:id]
     return if check_signed_in_user?
-    logger.info "hihi"
 
     if not is_higher_admin_b? @user
       flash[:error] = "You don't have privileges for user \"#{@user.name}\""

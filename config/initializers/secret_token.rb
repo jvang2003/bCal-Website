864817bef37ae99daf37d90ebf930ad1175b4377
Stephen Martinis <moowiz2020@gmail.com>
2013-12-06 17:43:11 -0800

    Changed secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -11,5 +11,5 @@
 BCalIntegration::Application.config.secret_token = if Rails.env.development? or Rails.env.test?
   'a172e66493427aee984d01e189e4a5be668e79488b15fd57de35da76b0a8c947423caa0e4da7657aa6a324c2522ddc5c5f97e4a70fa7b086576713588956dad7'
 else
-  ENV['SECRET_TOKEN']
+  ENV['SECRET_TOKEN'] || "07c58d8c35f1f1d97c579a4334b70a026a2885705a3e2684def83ef45b3a0b37dc3cf47dfd10ad374701a261324357e3f35eb8991074bec5666513ab592c6926"
 end

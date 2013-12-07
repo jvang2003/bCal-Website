8c82dcf6372ef831eb0f1f99fa46865540f7e191
Patrick Fong <patrickf3139@gmail.com>
2013-12-07 01:14:17 -0800

    added warning when using hard coded secret token in deployment. made field label for num attendees in new request view more clear. deleted useless files!

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -8,8 +8,14 @@
 # improving security of our app in deployment.
 # if this change created a bug, check out the post here
 # http://daniel.fone.net.nz/blog/2013/05/20/a-better-way-to-manage-the-rails-secret-token/
-BCalIntegration::Application.config.secret_token = if Rails.env.development? or Rails.env.test?
-  'a172e66493427aee984d01e189e4a5be668e79488b15fd57de35da76b0a8c947423caa0e4da7657aa6a324c2522ddc5c5f97e4a70fa7b086576713588956dad7'
+
+if Rails.env.development? or Rails.env.test?
+  token = 'a172e66493427aee984d01e189e4a5be668e79488b15fd57de35da76b0a8c947423caa0e4da7657aa6a324c2522ddc5c5f97e4a70fa7b086576713588956dad7'
+elsif ENV['SECRET_TOKEN'] == nil or ENV['SECRET_TOKEN'].length < 30
+	logger.warn "USING HARD CODED SECRET TOKEN IN DEPLOYMENT"
+	puts "USING HARD CODED SECRET TOKEN IN DEPLOYMENT"
+  token = "07c58d8c35f1f1d97c579a4334b70a026a2885705a3e2684def83ef45b3a0b37dc3cf47dfd10ad374701a261324357e3f35eb8991074bec5666513ab592c6926"
 else
-  ENV['SECRET_TOKEN'] || "07c58d8c35f1f1d97c579a4334b70a026a2885705a3e2684def83ef45b3a0b37dc3cf47dfd10ad374701a261324357e3f35eb8991074bec5666513ab592c6926"
+	token = ENV['SECRET_TOKEN']
 end
+BCalIntegration::Application.config.secret_token = token

7dfdbaaa375ad8e365a0701a698e95baa070acba
Bryan Chu <bryanchu08@yahoo.com>
2013-12-09 17:37:29 -0800

    form for blocks

--- app/controllers/blocked_times_controller.rb
+++ app/controllers/blocked_times_controller.rb
@@ -16,6 +16,8 @@ class BlockedTimesController < ApplicationController
   end
 
   def new
+    @calendar = Calendar.find(params[:calendar_id])
+    @blocked_time = BlockedTimes.new
   end
 
   def update

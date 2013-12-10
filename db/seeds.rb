a5702b7cdef6382255d36465bb0855a46a7bb74d
Stephen Martinis <moowiz2020@gmail.com>
2013-12-10 00:04:52 -0800

    Added user seed. Added string initializer.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create :name => "Andy Admin", :role => 3, :email => "admin@example.com"

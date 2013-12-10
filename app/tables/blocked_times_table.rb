6c0a3712029a776b61dc2ba4bdf553ad959a498d
Bryan Chu <bryanchu08@yahoo.com>
2013-12-09 23:54:06 -0800

    semi working views for blocks

--- app/tables/blocked_times_table.rb
+++ app/tables/blocked_times_table.rb
@@ -6,6 +6,16 @@ class BlockedTimesTable < TableCloth::Base
 
   column :start_time, :end_time
 
-  common_actions
+  class_eval do
+      include TableCloth::Extensions::Actions
+      actions do
+        action do |object|
+          link_to "Update", edit_calendar_blocked_time_path(object.calendar_id, object.id), :class => 'btn btn-info update-status', :id => "update_#{object.id}"
+        end
+        action do |object|
+          link_to "Delete", calendar_blocked_time_path(object.calendar_id, object.id), :class => 'btn btn-danger delete-status', :method => :delete, :id => "delete_#{object.id}"
+        end
+      end
+    end
 end
 
